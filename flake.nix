e5bd88e5e30f23f96494bc64f012eeb8d32f79c5
Carlos <carloscamposherrera446@gmail.com>
2023-06-20 21:34:39 +0200

    Fixed code lint

--- flake.nix
+++ flake.nix
@@ -11,48 +11,48 @@
     };
   };
 
-  outputs = {
-    nixpkgs,
-    home-manager,
-    ...
-  }: let
-    system = "x86_64-linux";
+  outputs =
+    { nixpkgs
+    , home-manager
+    , ...
+    }:
+    let
+      system = "x86_64-linux";
 
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {allowUnfree = true;};
-    };
-
-    lib = nixpkgs.lib;
-
-    base = [
-      ./system/configuration.nix
-      ./system/flatpak.nix
-    ];
-
-    g14 = [
-      ./system/nvidia.nix
-      ./system/g14.nix
-
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.cch = import ./home/home.nix;
-
-        # Optionally, use home-manager.extraSpecialArgs to pass
-        # arguments to home.nix
-      }
-    ];
-  in {
-    nixosConfigurations = {
-      g14 = lib.nixosSystem {
+      pkgs = import nixpkgs {
         inherit system;
-
-        modules = base ++ g14;
+        config = { allowUnfree = true; };
       };
-    };
 
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
-  };
+      base = [
+        ./system/configuration.nix
+        ./system/flatpak.nix
+      ];
+
+      g14 = [
+        ./system/nvidia.nix
+        ./system/g14.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.cch = import ./home/home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
+      ];
+    in
+    {
+      nixosConfigurations = {
+        g14 = nixpkgs.lib.nixosSystem {
+          inherit system;
+
+          modules = base ++ g14;
+        };
+      };
+
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+    };
 }
