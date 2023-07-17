f6c747dd8d12631045666dafedcf02b1dd2fb940
Carlos <carloscamposherrera446@gmail.com>
2023-07-18 01:01:37 +0200

    vscodium config

--- modules/users/cch/vscodium.nix
+++ modules/users/cch/vscodium.nix
@@ -1,18 +1,65 @@
 { pkgs
 , ...
 }: {
+
+  home.packages = with pkgs; [
+    cargo
+    rustc
+    rustfmt
+    gcc
+    jdk
+    nixpkgs-fmt
+  ];
+
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
     mutableExtensionsDir = true;
+    enableUpdateCheck = false;
+    enableExtensionUpdateCheck = false;
+
+    extensions = with pkgs.vscode-extensions; [
+      jnoortheen.nix-ide
+      vscodevim.vim
+      valentjn.vscode-ltex
+      redhat.java
+      jdinhlife.gruvbox
+      matklad.rust-analyzer
+    ];
+
     userSettings = {
-      "workbench.colorTheme" = "Gruvbox Material Dark";
-      "window.zoomLevel" = -1;
-      "files.autoSave" = "afterDelay";
-      "window.menuBarVisibility" = "toggle";
-      "editor.minimap.enabled" = false;
-      "editor.fontFamily" = "JetBrainsMono Nerd Font";
-      "editor.fontLigatures" = true;
+      workbench.colorTheme = "Gruvbox Dark Medium";
+      files.autoSave = "afterDelay";
+      window.menuBarVisibility = "toggle";
+
+      editor = {
+        minimap.enabled = false;
+        fontFamily = "JetBrainsMono Nerd Font";
+        fontLigatures = true;
+      };
+
+      nix = {
+        enableLanguageServer = true;
+        serverPath = "nil";
+        serverSettings = {
+          nil = {
+            formatting = {
+              command = [ "nixpkgs-fmt" ];
+            };
+          };
+        };
+      };
+
+      java = {
+        configuration.runtimes = [
+          {
+            name = "JavaSE-19";
+            path = "${pkgs.jdk}/lib/openjdk";
+            default = true;
+          }
+        ];
+        jdt.ls.java.home = "${pkgs.jdk}/lib/openjdk";
+      };
     };
   };
 }
