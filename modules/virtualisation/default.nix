ce38d16a748ff93f7b7e881110846be8adf6594b
Carlos <carloscamposherrera446@gmail.com>
2023-08-13 22:57:50 +0200

    virtualisation: added virt-manager

--- modules/virtualisation/default.nix
+++ modules/virtualisation/default.nix
@@ -1,8 +1,16 @@
-{ ... }: {
+{ pkgs
+, ...
+}: {
   virtualisation = {
     podman = {
       enable = true;
       dockerCompat = true;
     };
+
+    libvirtd.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [ virt-manager ];
+
+  users.users.cch.extraGroups = [ "libvirtd" ];
 }
