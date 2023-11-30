b03607ff7d79fbb2026a2dc722d81e02a73d073b
Carlos <carloscamposherrera446@gmail.com>
2023-11-30 20:14:27 +0100

    core: disable NetworkManager-wait-online

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -36,6 +36,10 @@
 
   networking.networkmanager.enable = true;
 
+  # DISABLE once and for all this useless and annoying service
+  systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+  systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
+
   # Configure console keymap
   console.keyMap = "es";
 
