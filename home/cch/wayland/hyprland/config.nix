0530ba0a0f68799394572246a400853162dc036d
Carlos <carloscamposherrera446@gmail.com>
2024-01-19 20:02:59 +0100

    hyprland: bring back show/hide on mainMod

--- home/cch/wayland/hyprland/config.nix
+++ home/cch/wayland/hyprland/config.nix
@@ -152,7 +152,7 @@ in {
         "$mainMod, mouse_down, workspace, e+1"
         "$mainMod, mouse_up, workspace, e-1"
       ];
-      bindr = "SUPER, SUPER_L, exec, wofi --show drun -a";
+      bindr = "SUPER, SUPER_L, exec, pkill wofi || wofi --show drun -a";
       bindm = ["$mainMod, mouse:272, movewindow" "$mainMod, mouse:273, resizewindow"];
     };
   };
