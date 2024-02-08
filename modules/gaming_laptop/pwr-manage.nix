3a22154efba812487d7c44b042e68b8a2b73a086
Carlos <carloscamposherrera446@gmail.com>
2024-02-08 23:43:05 +0100

    pwr-manage: smol refactor

--- modules/gaming_laptop/pwr-manage.nix
+++ modules/gaming_laptop/pwr-manage.nix
@@ -8,9 +8,9 @@
     text = builtins.readFile ./pwr-manage.sh;
     runtimeInputs = with pkgs; [power-profiles-daemon inotify-tools toybox];
   };
+
+  unplug = ''ACTION=="change", SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="0", RUN+="${lib.getExe pwrprofilecycle}"'';
+  plug = ''ACTION=="change", SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="1", RUN+="${lib.getExe pwrprofilecycle}"'';
 in {
-  services.udev.extraRules = ''
-    ACTION=="change", SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="0", RUN+="${lib.getExe pwrprofilecycle}"
-    ACTION=="change", SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="1", RUN+="${lib.getExe pwrprofilecycle}"
-  '';
+  services.udev.extraRules = lib.strings.concatLines [unplug plug];
 }
