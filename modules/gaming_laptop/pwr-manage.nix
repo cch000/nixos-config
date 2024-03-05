1cd2a73f4ac59784003862e22e395380f7596670
Carlos <carloscamposherrera446@gmail.com>
2024-03-05 17:16:43 +0100

    pwr-manage: fix incorrect script name

--- modules/gaming_laptop/pwr-manage.nix
+++ modules/gaming_laptop/pwr-manage.nix
@@ -3,14 +3,14 @@
   lib,
   ...
 }: let
-  pwrprofilecycle = pkgs.writeShellApplication {
-    name = "pwrprofilecycle";
+  pwr-manage = pkgs.writeShellApplication {
+    name = "pwr-manage";
     text = builtins.readFile ./pwr-manage.sh;
-    runtimeInputs = with pkgs; [power-profiles-daemon inotify-tools toybox];
+    runtimeInputs = with pkgs; [power-profiles-daemon toybox];
   };
 
-  unplug = ''ACTION=="change", KERNEL=="AC0", SUBSYSTEM=="power_supply", ATTR{online}=="0", RUN+="${lib.getExe pwrprofilecycle}"'';
-  plug = ''ACTION=="change", KERNEL=="AC0", SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="${lib.getExe pwrprofilecycle}"'';
+  unplug = ''ACTION=="change", KERNEL=="AC0", SUBSYSTEM=="power_supply", ATTR{online}=="0", RUN+="${lib.getExe pwr-manage}"'';
+  plug = ''ACTION=="change", KERNEL=="AC0", SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="${lib.getExe pwr-manage}"'';
 in {
   services.udev.extraRules = lib.strings.concatLines [unplug plug];
 }
--- modules/gaming_laptop/pwr-manage.sh
+++ modules/gaming_laptop/pwr-manage.sh
@@ -4,7 +4,7 @@ connected="$(echo /sys/class/power_supply/A*)/online"
 kernel_version=$(uname -r | cut -c1-3)
 readonly min_version=6.3
 
-#check if the laptop is plugged
+#check if the laptop is plugged in
 if [[ $(cat "$connected") == "0" ]]; then
 
   driver="passive"
@@ -25,12 +25,12 @@ fi
 
 echo "$log" | systemd-cat -t pwr-manage
 
-# Set cpu scheduling driver
+# Set cpu scheduling driver if we are on a supported kernel
 if (($(echo "$kernel_version >= $min_version" | bc -l))); then
   echo "$driver" | tee /sys/devices/system/cpu/amd_pstate/status
 fi
 
-# Set cpu scheduling governor if we are on a supported kernel
+# Set cpu scheduling governor
 for i in /sys/devices/system/cpu/*/cpufreq/scaling_governor; do
   echo "$governor" | tee "$i" >/dev/null
 done
