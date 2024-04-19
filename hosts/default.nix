1ec7d0f7fde65afd0fa9cf8e451f7a6636f156c8
Carlos <carloscamposherrera446@gmail.com>
2024-04-19 16:07:18 +0200

    hosts: function to make adding new hosts less repetitive

--- hosts/default.nix
+++ hosts/default.nix
@@ -9,24 +9,30 @@
   nvidia = ../imports/nvidia;
   client = ../imports/client;
   laptop = ../imports/client/laptop;
-  modules = ../modules;
+  options = ../modules;
   hm_module = inputs.home-manager.nixosModules.home-manager;
-in {
-  athena = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [
-      {networking.hostName = "athena";}
-      ./athena
-      modules
-      nvidia
-      core
-      client
-      laptop
-      hm_module
-    ];
-    specialArgs = {
-      inherit self inputs;
-      username = "cch";
+
+  mkHost = hostName: config:
+    nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        [
+          {networking = {inherit hostName;};}
+          ./${hostName}
+          options
+          core
+          hm_module
+        ]
+        ++ config.addModules;
+      specialArgs = {
+        inherit inputs;
+        inherit (config) username;
+      };
     };
-  };
-}
+in
+  builtins.mapAttrs mkHost {
+    athena = {
+      username = "cch";
+      addModules = [laptop client nvidia];
+    };
+  }
