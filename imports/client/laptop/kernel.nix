e8e971faf498251f15244ac7f3089f32a069b1a5
Carlos <carloscamposherrera446@gmail.com>
2024-05-02 22:18:19 +0200

    chore. formatting

--- imports/client/laptop/kernel.nix
+++ imports/client/laptop/kernel.nix
@@ -6,52 +6,53 @@
   custom = let
     baseKernel = pkgs.linux;
     suffix = "-bore";
-  in pkgs.linuxManualConfig {
-    inherit (baseKernel) src;
-    modDirVersion = "${baseKernel.modDirVersion}${suffix}";
-    version = "${baseKernel.version}${suffix}";
-    allowImportFromDerivation = true;
+  in
+    pkgs.linuxManualConfig {
+      inherit (baseKernel) src;
+      modDirVersion = "${baseKernel.modDirVersion}${suffix}";
+      version = "${baseKernel.version}${suffix}";
+      allowImportFromDerivation = true;
 
-    # generated using modprobed-db and
-    # make LSMOD=$HOME/.config/modprobed.db localmodconfig
-    configfile =
-    #https://github.com/NixOS/nixpkgs/issues/307014
-      builtins.toPath
-      (builtins.toFile "config" (
-        lib.strings.concatLines [
-          (builtins.readFile ./config)
-          "CONFIG_MZEN3=y"
-        ]
-      ));
+      # generated using modprobed-db and
+      # make LSMOD=$HOME/.config/modprobed.db localmodconfig
+      configfile =
+        #https://github.com/NixOS/nixpkgs/issues/307014
+        builtins.toPath
+        (builtins.toFile "config" (
+          lib.strings.concatLines [
+            (builtins.readFile ./config)
+            "CONFIG_MZEN3=y"
+          ]
+        ));
 
-    kernelPatches = [
-      {
-        name = "BORE CPU scheduler";
-        patch = pkgs.fetchurl {
-          url =
-            "https://raw.githubusercontent.com"
-            + "/firelzrd/bore-scheduler"
-            + "/main/patches/stable"
-            + "/linux-6.6-bore/0001-linux6.6.y-bore5.1.0.patch";
-          hash = "sha256-iLydPGZZSkEQhSj6Ah0Xq0zf7YUPwcpyKt8t0BeHYz8=";
-        };
-      }
-      {
-        name = "Native CPU optimizations";
-        patch = pkgs.fetchurl {
-          url =
-            "https://raw.githubusercontent.com"
-            + "/graysky2/kernel_compiler_patch"
-            + "/master/more-uarches-for-kernel-6.1.79-6.8-rc3.patch";
-          hash = "sha256-Gjglt5BBPQmAbJohFfZ5vijkNM/MacAdwGm2NNHoAHo=";
-        };
-        #extraStructuredConfig = with lib.kernel; {
-        #  GENERIC_CPU = mkForce no;
-        #  MZEN3 = mkForce yes;
-        #};
-      }
-    ];
-  };
+      kernelPatches = [
+        {
+          name = "BORE CPU scheduler";
+          patch = pkgs.fetchurl {
+            url =
+              "https://raw.githubusercontent.com"
+              + "/firelzrd/bore-scheduler"
+              + "/main/patches/stable"
+              + "/linux-6.6-bore/0001-linux6.6.y-bore5.1.0.patch";
+            hash = "sha256-iLydPGZZSkEQhSj6Ah0Xq0zf7YUPwcpyKt8t0BeHYz8=";
+          };
+        }
+        {
+          name = "Native CPU optimizations";
+          patch = pkgs.fetchurl {
+            url =
+              "https://raw.githubusercontent.com"
+              + "/graysky2/kernel_compiler_patch"
+              + "/master/more-uarches-for-kernel-6.1.79-6.8-rc3.patch";
+            hash = "sha256-Gjglt5BBPQmAbJohFfZ5vijkNM/MacAdwGm2NNHoAHo=";
+          };
+          #extraStructuredConfig = with lib.kernel; {
+          #  GENERIC_CPU = mkForce no;
+          #  MZEN3 = mkForce yes;
+          #};
+        }
+      ];
+    };
 
   #https://github.com/NixOS/nixpkgs/issues/216529
   passthru = {
