3567f271d84cf47a4f4f88cabdf71516399d2c59
Carlos <carloscamposherrera446@gmail.com>
2024-05-19 23:09:51 +0200

    laptop: replace asusctl -c

--- imports/client/laptop/power.nix
+++ imports/client/laptop/power.nix
@@ -3,10 +3,77 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  inherit (lib) getExe;
+  batCapPath = "/home/cch/.config/bat-cap";
+in {
   imports = [
     inputs.power-cap-rs.nixosModules.pwr-cap-rs
   ];
+
+  environment.systemPackages = let
+    bat-cap = pkgs.writeShellApplication {
+      name = "bat-cap";
+      text = ''
+        path=${batCapPath}
+        var=''${1-default}
+        if [ "$var" == default ]; then
+          cat "$path"
+        else
+          echo "$var" > "$path"
+        fi
+      '';
+    };
+  in [bat-cap];
+
+  systemd = {
+    paths.bat-cap = {
+      pathConfig = {
+        PathChanged = batCapPath;
+      };
+      wantedBy = ["multi-user.target"];
+    };
+
+    services = {
+      bat-cap = let
+        bat-cap = pkgs.writeShellApplication {
+          name = "bat-cap";
+          text = ''
+            percentage=$(cat ${batCapPath})
+            echo "$percentage" | tee /sys/class/power_supply/BAT0/charge_control_end_threshold
+          '';
+        };
+      in {
+        description = "set maximum battery percentage";
+
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+          ExecStart = getExe bat-cap;
+        };
+
+        wantedBy = ["multi-user.target"];
+      };
+
+      pwr-manage = let
+        pwr-manage = pkgs.writeShellApplication {
+          name = "pwr-manage";
+          text = builtins.readFile ./pwr-manage.sh;
+          runtimeInputs = with pkgs; [toybox];
+        };
+      in {
+        description = "power tweaks when unplugged";
+
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+          ExecStart = getExe pwr-manage;
+        };
+
+        after = ["power-profiles-daemon.service"];
+      };
+    };
+  };
 
   services = {
     pwr-cap-rs = {
@@ -21,14 +88,9 @@
     switcherooControl.enable = true;
     power-profiles-daemon.enable = true;
     udev.extraRules = let
-      pwr-manage = pkgs.writeShellApplication {
-        name = "pwr-manage";
-        text = builtins.readFile ./pwr-manage.sh;
-        runtimeInputs = with pkgs; [toybox];
-      };
-
-      unplug = ''ACTION=="change", KERNEL=="AC0", SUBSYSTEM=="power_supply", ATTR{online}=="0", RUN+="${lib.getExe pwr-manage}"'';
-      plug = ''ACTION=="change", KERNEL=="AC0", SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="${lib.getExe pwr-manage}"'';
+      command = "${lib.getExe' pkgs.systemd "systemctl"} start pwr-manage.service";
+      unplug = ''ACTION=="change", KERNEL=="AC0", SUBSYSTEM=="power_supply", ATTR{online}=="0", RUN+="${command}"'';
+      plug = ''ACTION=="change", KERNEL=="AC0", SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="${command}"'';
     in
       lib.strings.concatLines [unplug plug];
   };
