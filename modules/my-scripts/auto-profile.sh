20b41ff77232f90d726420383abb7aa2a4eba4e7
Carlos <carloscamposherrera446@gmail.com>
2024-06-18 00:57:53 +0200

    auto-profile: formatting

--- modules/my-scripts/auto-profile.sh
+++ modules/my-scripts/auto-profile.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 
-
 while true; do
 
-    connected="$(cat /sys/class/power_supply/A*/online)"
+  connected="$(cat /sys/class/power_supply/A*/online)"
 
-    if [ "$connected" == "1" ]; then
+  if [ "$connected" == "1" ]; then
 
-        prev=$(powerprofilesctl get)
-        profile=$prev
-        load1min=$(LC_NUMERIC="en_US.UTF-8" printf %.0f "$(cut -d" " -f1 /proc/loadavg)")
+    prev=$(powerprofilesctl get)
+    profile=$prev
+    load1min=$(LC_NUMERIC="en_US.UTF-8" printf %.0f "$(cut -d" " -f1 /proc/loadavg)")
 
-        if [[ load1min -lt 7 && $(supergfxctl -S) != "active" ]]; then
+    if [[ load1min -lt 7 && $(supergfxctl -S) != "active" ]]; then
 
-            profile="power-saver"
+      profile="power-saver"
 
-        fi
-
-        if [ "$prev" != "$profile" ]; then
-            powerprofilesctl set "$profile"
-
-            # Update waybar icon if waybar is running
-            if pidof waybar >/dev/null; then
-                pkill -SIGRTMIN+8 waybar
-            fi
-        fi
     fi
 
-    sleep 300
+    if [ "$prev" != "$profile" ]; then
+      powerprofilesctl set "$profile"
+
+      # Update waybar icon if waybar is running
+      if pidof waybar >/dev/null; then
+        pkill -SIGRTMIN+8 waybar
+      fi
+    fi
+  fi
+
+  sleep 300
 
 done
