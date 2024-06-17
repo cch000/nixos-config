3025555763ec085596706ac2d57200ca235d82ea
Carlos <carloscamposherrera446@gmail.com>
2024-06-18 00:55:38 +0200

    nvidia: refactor + use old driver

--- imports/nvidia/default.nix
+++ imports/nvidia/default.nix
@@ -1,14 +1,20 @@
-_:
-#example of let block and package version override
-# let
-#  nvidia-535-129 = config.boot.kernelPackages.nvidiaPackages.stable.overrideAttrs {
-#    src = pkgs.fetchurl {
-#      url = "https://download.nvidia.com/XFree86/Linux-x86_64/535.129.03/NVIDIA-Linux-x86_64-535.129.03.run";
-#      sha256 = "sha256-5tylYmomCMa7KgRs/LfBrzOLnpYafdkKwJu4oSb/AC4=";
-#    };
-#  };
-#in
 {
+  config,
+  pkgs,
+  ...
+}:
+#example of let block and package version override
+let
+  version = "535.129.03";
+  my-nvidia = config.boot.kernelPackages.nvidiaPackages.stable.overrideAttrs {
+    inherit version;
+    name = "nvidia-${version}";
+    src = pkgs.fetchurl {
+      url = "https://download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run";
+      sha256 = "sha256-5tylYmomCMa7KgRs/LfBrzOLnpYafdkKwJu4oSb/AC4=";
+    };
+  };
+in {
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware = {
@@ -17,7 +23,8 @@ _:
       open = false;
       powerManagement.enable = true;
       dynamicBoost.enable = true;
-      # package = nvidia-535-129;
+      nvidiaSettings = false;
+      package = my-nvidia;
     };
   };
 }
