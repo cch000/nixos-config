40f0541b8d1427af63f9a2591b1cdc90f2ebee6b
Carlos <carloscamposherrera446@gmail.com>
2024-07-03 01:00:03 +0200

    nvidia: use latest stable drivers

--- imports/nvidia/default.nix
+++ imports/nvidia/default.nix
@@ -1,20 +1,17 @@
-{
-  config,
-  pkgs,
-  ...
-}:
+_:
 #example of let block and package version override
-let
-  version = "535.129.03";
-  my-nvidia = config.boot.kernelPackages.nvidiaPackages.stable.overrideAttrs {
-    inherit version;
-    name = "nvidia-${version}";
-    src = pkgs.fetchurl {
-      url = "https://download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run";
-      sha256 = "sha256-5tylYmomCMa7KgRs/LfBrzOLnpYafdkKwJu4oSb/AC4=";
-    };
-  };
-in {
+#let
+#  version = "535.129.03";
+#  my-nvidia = config.boot.kernelPackages.nvidiaPackages.stable.overrideAttrs {
+#    inherit version;
+#    name = "nvidia-${version}";
+#    src = pkgs.fetchurl {
+#      url = "https://download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run";
+#      sha256 = "sha256-5tylYmomCMa7KgRs/LfBrzOLnpYafdkKwJu4oSb/AC4=";
+#    };
+#  };
+#in
+{
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware = {
@@ -24,7 +21,7 @@ in {
       powerManagement.enable = true;
       dynamicBoost.enable = true;
       nvidiaSettings = false;
-      package = my-nvidia;
+      #package = my-nvidia;
     };
   };
 }
