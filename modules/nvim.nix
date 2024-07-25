9ee72222888f8575d44ce90bc04fac1fcc1a1548
Carlos <carloscamposherrera446@gmail.com>
2024-07-25 13:31:50 +0200

    nvim: add spellcheck to git commits

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -70,6 +70,7 @@ in {
             set spelllang=en_us
             set colorcolumn=80
             set termguicolors
+            autocmd FileType gitcommit setlocal spell
 
             "Close folds
             nmap <S-f> :foldclose<CR>
