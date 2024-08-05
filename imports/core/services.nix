6d9e687883b5d3d7d7b097d01e4fab98ebe8f0c6
Carlos <carloscamposherrera446@gmail.com>
2024-08-05 16:12:02 +0200

    services: enable automatic block group reclaim on btrfs

--- imports/core/services.nix
+++ imports/core/services.nix
@@ -3,13 +3,23 @@
   systemd.services = {
     NetworkManager-wait-online.enable = lib.mkForce false;
     systemd-networkd-wait-online.enable = lib.mkForce false;
+
+    set-bg_reclaim_threshold = {
+      description = "enable automatic block group reclaim on sysfs";
+
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+        ExecStart = "/bin/sh -c 'echo 10 > /sys/fs/btrfs/50503012-89a6-47cc-bed6-821e657cf00f/allocation/data/bg_reclaim_threshold'";
+      };
+
+      wantedBy = ["multi-user.target"];
+    };
   };
 
   services = {
     btrfs.autoScrub.enable = true;
-
     fstrim.enable = true;
-
     fwupd.enable = true;
   };
 
