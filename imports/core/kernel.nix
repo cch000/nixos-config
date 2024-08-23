aeb8d767228f4cc27ba26ba81074084c76c38911
Carlos <carloscamposherrera446@gmail.com>
2024-08-23 14:31:39 +0200

    kernel: enable sysrq

--- imports/core/kernel.nix
+++ imports/core/kernel.nix
@@ -47,8 +47,7 @@
     kernel.sysctl = {
       # The Magic SysRq key is a key combo that allows users connected to the
       # system console of a Linux kernel to perform some low-level commands.
-      # Disable it, since we don't need it, and is a potential security concern.
-      "kernel.sysrq" = 0;
+      "kernel.sysrq" = 1;
 
       "kernel.exec-shield" = 1;
       # No core dump of executable setuid
