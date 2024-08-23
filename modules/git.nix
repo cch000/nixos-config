5e030d66e45c5ac8516b000779aa33342e9753c0
Carlos <carloscamposherrera446@gmail.com>
2024-08-23 14:32:20 +0200

    git: disable commit signing by default

--- modules/git.nix
+++ modules/git.nix
@@ -16,10 +16,10 @@ in {
       enable = true;
       userName = "Carlos";
       userEmail = "carloscamposherrera446@gmail.com";
-      signing = {
-        key = "E56E50F86E9F5DC9";
-        signByDefault = true;
-      };
+      #signing = {
+      #  key = "E56E50F86E9F5DC9";
+      #  signByDefault = true;
+      #};
 
       extraConfig = {
         core = {
