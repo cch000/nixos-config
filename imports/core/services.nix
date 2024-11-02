c088920b2661559ed4481ea255ad43ae0b05f742
Carlos <carloscamposherrera446@gmail.com>
2024-11-02 19:24:52 +0100

    services: enable oomd for user and root slices

--- imports/core/services.nix
+++ imports/core/services.nix
@@ -1,19 +1,26 @@
 {lib, ...}: {
   # DISABLE once and for all this useless and annoying service
-  systemd.services = {
-    NetworkManager-wait-online.enable = lib.mkForce false;
-    systemd-networkd-wait-online.enable = lib.mkForce false;
+  systemd = {
+    services = {
+      NetworkManager-wait-online.enable = lib.mkForce false;
+      systemd-networkd-wait-online.enable = lib.mkForce false;
 
-    set-bg_reclaim_threshold = {
-      description = "enable automatic block group reclaim on sysfs";
+      set-bg_reclaim_threshold = {
+        description = "enable automatic block group reclaim on sysfs";
 
-      serviceConfig = {
-        Type = "oneshot";
-        User = "root";
-        ExecStart = "/bin/sh -c 'echo 10 > /sys/fs/btrfs/566d65d0-c4f4-4e73-a8fb-71222a5fa88d/allocation/data/bg_reclaim_threshold'";
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+          ExecStart = "/bin/sh -c 'echo 10 > /sys/fs/btrfs/566d65d0-c4f4-4e73-a8fb-71222a5fa88d/allocation/data/bg_reclaim_threshold'";
+        };
+
+        wantedBy = ["multi-user.target"];
       };
-
-      wantedBy = ["multi-user.target"];
+    };
+    oomd = {
+      enable = true;
+      enableUserSlices = true;
+      enableRootSlice = true;
     };
   };
 
