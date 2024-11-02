cc4001af560abf15970244cc71e79599894ab18a
Carlos <carloscamposherrera446@gmail.com>
2024-11-02 15:15:54 +0100

    bluetooth: improve headphone controls support

--- imports/client/laptop/bluetooth.nix
+++ imports/client/laptop/bluetooth.nix
@@ -1,8 +1,24 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  bluez = pkgs.bluez-experimental;
+in {
   hardware.bluetooth = {
     enable = true;
-    package = pkgs.bluez-experimental;
+    package = bluez;
   };
+  systemd.user.services.mpris-proxy = {
+    enable = true;
+    unitConfig = {
+      Description = "Proxy forwarding Bluetooth MIDI controls via MPRIS2 to control media players";
+      BindsTo = ["bluetooth.target"];
+      After = ["bluetooth.target"];
+    };
 
+    wantedBy = ["bluetooth.target"];
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${bluez}/bin/mpris-proxy";
+    };
+  };
   services.blueman.enable = true;
 }
