cdd4bce2a34d184f2c1d84898ce8db58a79fc490
Carlos <carloscamposherrera446@gmail.com>
2024-11-04 00:52:54 +0100

    kernel: config updates

--- imports/client/laptop/kernel.nix
+++ imports/client/laptop/kernel.nix
@@ -20,8 +20,23 @@
         builtins.toPath
         (builtins.toFile "config" (
           lib.strings.concatLines [
-            (builtins.readFile ./config)
+            (builtins.replaceStrings
+              #from
+              [
+                "CONFIG_X86_EXTENDED_PLATFORM=y"
+                "CONFIG_X86_MCE_INTEL=y"
+                "CONFIG_PREEMPT_VOLUNTARY=y"
+              ]
+              #to
+              [
+                "CONFIG_X86_EXTENDED_PLATFORM=n"
+                "CONFIG_X86_MCE_INTEL=n"
+                "CONFIG_PREEMPT_VOLUNTARY=n"
+              ]
+              (builtins.readFile ./config))
+            #add
             "CONFIG_MZEN3=y"
+            "CONFIG_PREEMPT=y"
           ]
         ));
 
@@ -32,9 +47,51 @@
             url =
               "https://raw.githubusercontent.com"
               + "/firelzrd/bore-scheduler"
-              + "/main/patches/stable"
-              + "/linux-6.6-bore/0001-linux6.6.30-bore5.6.1.patch";
-            hash = "sha256-aWkhsiZWyPVmtTiG4Kf9oTgelz4dXrednJop5Bc99Ao=";
+              + "/main/patches/stable/linux-6.6-bore"
+              + "/0001-linux6.6.57-bore5.7.2.patch";
+            hash = "sha256-UkwKs8latDnpNPhPsbvJ4wxu2buQhiWrLuv6u9JKF6g=";
+          };
+        }
+        {
+          name = "bbr v3";
+          patch = pkgs.fetchurl {
+            url =
+              "https://raw.githubusercontent.com"
+              + "/CachyOS/kernel-patches/refs/heads/master/6.6"
+              + "/0001-bbr3.patch";
+            hash = "sha256-hu/to3Oo/ppIRC/iczNxWdnmJ1dmTeA0yUaGULkfMlE=";
+          };
+        }
+
+        {
+          name = "zstd optimizations";
+          patch = pkgs.fetchurl {
+            url =
+              "https://raw.githubusercontent.com"
+              + "/CachyOS/kernel-patches"
+              + "/refs/heads/master/6.6/0005-zstd.patch";
+            hash = "sha256-onNd7OdkBnXYW3xyUVvxhXt0kqW5SQF1Wz2iJKVSLWk=";
+          };
+        }
+        # Provides syscalls for uksmd. uksmd does identify same memory pages and merges them
+        {
+          name = "ksm";
+          patch = pkgs.fetchurl {
+            url =
+              "https://raw.githubusercontent.com"
+              + "/CachyOS/kernel-patches/refs/heads/master/6.6/"
+              + "0004-ksm.patch";
+            hash = "sha256-4cnHLbjNxFGAxcYfCI28A+IoFcntxBXeG7eUj8cbQcA=";
+          };
+        }
+        {
+          name = "fixes";
+          patch = pkgs.fetchurl {
+            url =
+              "https://raw.githubusercontent.com"
+              + "/CachyOS/kernel-patches/refs/heads/master/6.6/"
+              + "0003-fixes.patch";
+            hash = "sha256-v6il4epclV6ehGhG+S8Lxqg3OR5HQGUz+EWFUZTnC7E=";
           };
         }
         {
